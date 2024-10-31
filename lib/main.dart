1a7485e750b787c1bc9b53053e9db95fe42a94a8
Raouf Rahiche <mrrahicheraouf@gmail.com>
2024-10-31 14:17:49 +0000

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:image_filters/live_coding/onboarding_with_blur.dart';
 import 'package:image_filters/live_coding/selective_focus.dart';
 import 'package:image_filters/sections/1_basics/basic_filters.dart';
 import 'package:image_filters/sections/2_combine/compose_filters.dart';
@@ -59,8 +60,8 @@ class HomeScreen extends StatelessWidget {
     ),
     DemoItem(
       title: '6. Onboarding with blur',
-      // content: const OnboardingWithBlur(),
-      content: const SelectiveFocus(),
+      content: const OnboardingWithBlur(),
+      // content: const SelectiveFocus(),
     ),
   ];
 
